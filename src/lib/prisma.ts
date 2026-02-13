47f7074ce371b0be9dcf24917639ab0ea19a3d02
alarv <alarvfm@gmail.com>
2026-02-13 23:50:42 +0200

    fix prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,18 @@
-import "dotenv/config";
+import { PrismaClient } from '../app/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client';
 
-const connectionString = `${process.env.DATABASE_URL}`
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient
+}
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+})
 
-export { prisma }
+const prisma = globalForPrisma.prisma || new PrismaClient({
+    adapter,
+})
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
